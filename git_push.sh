b5730cae0f7e612dcd603b00d2f9eb0d762f289c
Dan Chen <dan.chen@boardware.com>
2024-01-18 14:37:23 +0800

    feat

--- git_push.sh
+++ git_push.sh
@@ -33,27 +33,10 @@ fi
 # Initialize the local directory as a Git repository
 git init
 
-# Adds the files in the local repository and stages them for commit.
 git add .
 
 # Commits the tracked changes and prepares them to be pushed to a remote repository.
 git commit -m "$release_note"
 
-# Sets the new remote
-git_remote=$(git remote)
-if [ "$git_remote" = "" ]; then # git remote not defined
-
-    if [ "$git_token" = "" ]; then
-        echo "[INFO] \$GIT_TOKEN (environment variable) is not set. Using the git credential in your environment."
-        git remote add origin https://${git_host}/${git_user_id}/${git_repo_id}.git
-    else
-        git remote add origin https://${git_user_id}:"${git_token}"@${git_host}/${git_user_id}/${git_repo_id}.git
-    fi
-
-fi
-
-git pull origin main
-
-# Pushes (Forces) the changes in the local repository up to the remote repository
-echo "Git pushing to https://${git_host}/${git_user_id}/${git_repo_id}.git"
-git push origin main 2>&1 | grep -v 'To https'
+git remote add origin https://${git_user_id}:"${GIT_TOKEN}"@${git_host}/${git_user_id}/${git_repo_id}.git
+git git push -u origin main 2>&1 | grep -v 'To https'
